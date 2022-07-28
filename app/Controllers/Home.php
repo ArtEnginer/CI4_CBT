1930c38238b6eccdad2e96128ee7f01543d7136b
Angga Saputra <72662866+ArtEnginer@users.noreply.github.com>
2022-07-28 20:50:19 +0700

    settings

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -2,10 +2,23 @@
 
 namespace App\Controllers;
 
+
 class Home extends BaseController
 {
+    /**
+     * @var PanelConfig
+     */
+    protected $config;
+
+    public function __construct()
+    {
+        $this->config = config('Theme');
+        $this->data['config']   = $this->config;
+        $this->data['menuactive'] = 'dashboard';
+    }
+
     public function index()
     {
-        return view('welcome_message');
+        return view('Panel/Page/panel', $this->data);
     }
 }
